ee65dc1a33d6c00d675386a8d47fde307ab622d2
MarkG <mgraven.mark@gmail.com>
2024-12-10 12:08:50 -0800

    added comments to camera shake

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -7,23 +7,23 @@ public class CameraShake : MonoBehaviour
     // Start is called before the first frame update
     public IEnumerator Shake (float duration, float magnitude) //creating coroutine
     {
-        Vector3 originalPos = transform.localPosition;
+        Vector3 originalPos = transform.localPosition; //initializing original position
 
-        float elapsed = 0.0f;
+        float elapsed = 0.0f; //setting/resetting elapsed time
         
-        while (elapsed < duration)
+        while (elapsed < duration) //while the time passed is less than the max duration
         {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
+            float x = Random.Range(-1f, 1f) * magnitude; //random in range 
+            float y = Random.Range(-1f, 1f) * magnitude; //random in range
 
-            transform.localPosition = new Vector3(x, y, originalPos.z);
+            transform.localPosition = new Vector3(x, y, originalPos.z); //combining
 
-            elapsed += Time.deltaTime;
+            elapsed += Time.deltaTime; //incrementing elapsed time
 
-            yield return null;
+            yield return null; //corouting return
         }
 
-        transform.localPosition = originalPos;
+        transform.localPosition = originalPos; //resetting position after shake ends
 
     }
 }
